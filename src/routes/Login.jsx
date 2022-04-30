a0e58867f3401722d9ea52cfb6d47a8de18a2d51
Maximo-4ever <maximilianocm06@gmail.com>
2022-04-29 19:45:24 -0500

    Añade la ruta de visualizacion de Login

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,19 +1,44 @@
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../context/UserProvider";
 
 const Login = () => {
-  const { user, setUser } = useContext(UserContext);
+  const [email, setEmail] = useState("maximo@test.com");
+  const [password, setPassword] = useState("123456");
+
+  const { loginUser } = useContext(UserContext);
   const navigate = useNavigate();
-  const handleClickLogin = () => {
-    setUser(true);
-    navigate("/");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Procesando Form: ", email, password);
+    try {
+      console.log("Iniciando sesion");
+      await loginUser(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error.code);
+    }
   };
+
   return (
     <>
-      <h1>Loginnnnnnnnnnn</h1>
-      <p>{user ? "En linea" : "Desconectado"}</p>
-      <button onClick={handleClickLogin}>Acceder</button>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Ingrese email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Ingrese password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 };
